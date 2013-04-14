411e691eddf85d91f503785b855f4eb2f17c31c3
Bertrand Boustany <bertrand@boustany.me>
2013-04-14 12:58:40 +0200

    Cleaning up

---
+++ Font.js
@@ -0,0 +1,109 @@
+function Font() {
+// CONSTRUCTOR
+	if (Font.prototype._singletonInstance) {
+		return Font.prototype._singletonInstance;
+	}
+	Font.prototype._singletonInstance = this;
+//
+// PROPERTIES
+	var blockWidth = 5;
+	var leftPadding = 0;
+	var topPadding = 0;
+	var msg = "0xDEAD10CC";
+	var characters = {
+		'0': [
+		" XXX ",
+		"X   X",
+		"XX  X",
+		"X X X",
+		"X  XX",
+		"X   X",
+		" XXX "
+		],
+		'x':[
+		"     ",
+		"     ",
+		"X   X",
+		" X X ",
+		"  X  ",
+		" X X ",
+		"X   X"
+		],
+		'D': [
+		"XXXX ",
+		"X   X",
+		"X   X",
+		"X   X",
+		"X   X",
+		"X   X",
+		"XXXX "
+		],
+		'E': [
+		"XXXXX",
+		"X    ",
+		"X    ",
+		"XXX  ",
+		"X    ",
+		"X    ",
+		"XXXXX"
+		],
+		'A': [
+		" XXX ",
+		"X   X",
+		"X   X",
+		"X   X",
+		"XXXXX",
+		"X   X",
+		"X   X"
+		],
+		'1': [
+		"  X  ",
+		" XX  ",
+		"  X  ",
+		"  X  ",
+		"  X  ",
+		"  X  ",
+		"  X  ",
+		],
+		'C': [
+		" XXX ",
+		"X   X",
+		"X    ",
+		"X    ",
+		"X    ",
+		"X   X",
+		" XXX "
+		],
+	};
+//
+// METHODS
+	this.Update = function (timer) {
+		blockWidth += timer.GetSin() / 400;
+		leftPadding = config.screenWidth / 2 - ((msg.length * 6 + timer.GetSin()) * blockWidth - blockWidth) / 2;
+		topPadding = config.screenHeight / 2 - (7 + timer.GetCos()) * blockWidth / 2;
+	};
+	this.Draw = function (canvas) {
+		var ctx = canvas.GetContext();
+		ctx.beginPath();
+		ctx.strokeStyle = '#FFF';
+		ctx.lineWidth = blockWidth + 1;
+		ctx.lineJoin = 'miter';
+		ctx.fillStyle = '#000';
+		for (var i = 0; i < msg.length; i++) {
+			for (var j = 0; j < characters[msg[i]].length; j++) {
+				for (var k = 0; k < characters[msg[i]][j].length; k++) {
+					if (characters[msg[i]][j][k] === 'X') {
+						ctx.moveTo(leftPadding + (i * 6 * blockWidth) + k * blockWidth, topPadding + j * blockWidth);
+						ctx.lineTo(leftPadding + (i * 6 * blockWidth) + k * blockWidth + blockWidth, topPadding + j * blockWidth);
+						ctx.lineTo(leftPadding + (i * 6 * blockWidth) + k * blockWidth + blockWidth, topPadding + j * blockWidth + blockWidth);
+						ctx.lineTo(leftPadding + (i * 6 * blockWidth) + k * blockWidth, topPadding + j * blockWidth + blockWidth);
+						ctx.lineTo(leftPadding + (i * 6 * blockWidth) + k * blockWidth, topPadding + j * blockWidth);
+					}
+				}
+			}
+		}
+		ctx.stroke();
+		ctx.fill();
+	};
+//
+};
--- Game.js
+++ Game.js
@@ -1,122 +1,10 @@
-var inputs = new Inputs();
 var background = new Background();
 var cube = new Cube();
-var letters = {
-	blockWidth: 5,
-	leftPadding: 0,
-	topPadding: 0,
-	msg: "0xDEAD10CC",
-	font: {
-		'0': [
-		" XXX ",
-		"X   X",
-		"XX  X",
-		"X X X",
-		"X  XX",
-		"X   X",
-		" XXX "
-		],
-		'x':[
-		"     ",
-		"     ",
-		"X   X",
-		" X X ",
-		"  X  ",
-		" X X ",
-		"X   X"
-		],
-		'D': [
-		"XXXX ",
-		"X   X",
-		"X   X",
-		"X   X",
-		"X   X",
-		"X   X",
-		"XXXX "
-		],
-		'E': [
-		"XXXXX",
-		"X    ",
-		"X    ",
-		"XXX  ",
-		"X    ",
-		"X    ",
-		"XXXXX"
-		],
-		'A': [
-		" XXX ",
-		"X   X",
-		"X   X",
-		"X   X",
-		"XXXXX",
-		"X   X",
-		"X   X"
-		],
-		'1': [
-		"  X  ",
-		" XX  ",
-		"  X  ",
-		"  X  ",
-		"  X  ",
-		"  X  ",
-		"  X  ",
-		],
-		'C': [
-		" XXX ",
-		"X   X",
-		"X    ",
-		"X    ",
-		"X    ",
-		"X   X",
-		" XXX "
-		],
+var font = new Font();
 
-	},
-	Update: function (timer) {
-		this.blockWidth += timer.GetSin() / 400;
-		this.leftPadding = config.screenWidth / 2 - ((this.msg.length * 6 + timer.GetSin()) * this.blockWidth - this.blockWidth) / 2;
-		this.topPadding = config.screenHeight / 2 - (7 + timer.GetCos()) * this.blockWidth / 2;
-	},
-	Draw: function (canvas) {
-		var ctx = canvas.GetContext();
-		ctx.beginPath();
-		ctx.strokeStyle = '#FFF';
-		ctx.lineWidth = this.blockWidth + 1;
-		ctx.lineJoin = 'miter';
-		ctx.fillStyle = '#000';
-		for (var i = 0; i < this.msg.length; i++) {
-			for (var j = 0; j < this.font[this.msg[i]].length; j++) {
-				for (var k = 0; k < this.font[this.msg[i]][j].length; k++) {
-					if (this.font[this.msg[i]][j][k] === 'X') {
-						ctx.moveTo(this.leftPadding + (i * 6 * this.blockWidth) + k * this.blockWidth, this.topPadding + j * this.blockWidth);
-						ctx.lineTo(this.leftPadding + (i * 6 * this.blockWidth) + k * this.blockWidth + this.blockWidth, this.topPadding + j * this.blockWidth);
-						ctx.lineTo(this.leftPadding + (i * 6 * this.blockWidth) + k * this.blockWidth + this.blockWidth, this.topPadding + j * this.blockWidth + this.blockWidth);
-						ctx.lineTo(this.leftPadding + (i * 6 * this.blockWidth) + k * this.blockWidth, this.topPadding + j * this.blockWidth + this.blockWidth);
-						ctx.lineTo(this.leftPadding + (i * 6 * this.blockWidth) + k * this.blockWidth, this.topPadding + j * this.blockWidth);
-					}
-				}
-			}
-		}
-		ctx.stroke();
-		ctx.fill();
-	}
-};
 
 function Update (timer) {
 	timer.Update();
-	// if (inputs.IsPressed('left')) {
-	// 	cube.MoveLeft(config.mvmtSpeed * timer.GetElapsedTime());
-	// } if (inputs.IsPressed('up')) {
-	// 	cube.MoveUp(config.mvmtSpeed * timer.GetElapsedTime());
-	// } if (inputs.IsPressed('right')) {
-	// 	cube.MoveRight(config.mvmtSpeed * timer.GetElapsedTime());
-	// } if (inputs.IsPressed('down')) {
-	// 	cube.MoveDown(config.mvmtSpeed * timer.GetElapsedTime());
-	// } if (inputs.IsPressed('[')) {
-	// 	cube.ScaleDown(1 + 0.003 * timer.GetElapsedTime());
-	// } if (inputs.IsPressed(']')) {
-	// 	cube.ScaleUp(1 + 0.003 * timer.GetElapsedTime());
-	// }
 	cube.TurnX(config.speed / 3 * timer.GetElapsedTime());
 	cube.TurnY(config.speed / 3 * timer.GetElapsedTime());
 	cube.TurnZ(config.speed / 3 * timer.GetElapsedTime());
@@ -125,26 +13,21 @@ function Update (timer) {
 	} else {
 		cube.ScaleUp(1 + timer.GetSin() / 30);
 	}
-	// if (timer.GetCos() > 0) {
-		// console.log(timer.GetCos());
 	cube.SetPosX(config.screenWidth / 2 + timer.GetSin() * 200);
-	// } else {
-		// cube.MoveLeft(Math.abs(timer.GetCos() * 10));
-	// }
 	background.Update(timer);
 	cube.Update(timer);
-	letters.Update(timer);
+	font.Update(timer);
 }
 
 function Draw (canvas, timer) {
 	canvas.ClearScreen();
 	background.Draw(canvas);
 	if (timer.GetLastSin() > timer.GetSin()) {
-		letters.Draw(canvas);
+		font.Draw(canvas);
 		cube.Draw(canvas);
 	} else {
 		cube.Draw(canvas);
-		letters.Draw(canvas);
+		font.Draw(canvas);
 	}
 }
 
