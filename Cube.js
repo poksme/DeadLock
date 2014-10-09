77c53587834e7ba0460ae019964c9325c4e35a8c
Romain Deguy <rx@interaction.net.au>
2014-10-09 16:49:54 +0200

    +1 Cube

--- Cube.js
+++ Cube.js
@@ -103,7 +103,161 @@ function Cube () {
 			var YX = (XD + ZX) * Math.cos(angle.y) - ZD * Math.sin(angle.y) - (XD + ZX);
 			var YZ = (XD + ZX) * Math.sin(angle.y) + ZD * Math.cos(angle.y) - ZD;
 			var XY = (YD + ZY) * Math.cos(angle.x) - (ZD + YZ) * Math.sin(angle.x) - (YD + ZY);
-			var XZ = (YD + ZY) * Math.sin(angle.x) + (ZD + YZ) * Math.cos(angle.x) - (ZD + YZ);
+			var XZ = (YD + ZY) * 4 * Math.sin(angle.x) + (ZD + YZ) * Math.cos(angle.x) - (ZD + YZ);
+			return {
+				x: YX + ZX,
+				y: ZY + XY,
+				z: XZ + YZ
+			};
+		};
+		this.Update = function (timer) {
+			for (var i = 0; i < dimensions.length; i++) {
+				offsets[i] = GetOffset(dimensions[i]);
+			}
+		};
+	//
+	// DRAW
+		var MoveTo = function (ctx, index) {
+			ctx.moveTo(
+				(dimensions[index].x + offsets[index].x) / position.z + position.x,
+				(dimensions[index].y + offsets[index].y) / position.z + position.y
+			);
+		};
+		var LineTo = function (ctx, index) {
+			ctx.lineTo(
+				(dimensions[index].x + offsets[index].x) / position.z + position.x,
+				(dimensions[index].y + offsets[index].y) / position.z + position.y
+			);
+		};
+		this.Draw = function (canvas) {
+			var ctx = canvas.GetContext();
+			ctx.beginPath();
+			ctx.strokeStyle = '#BAEF03';
+			ctx.lineWidth = thickness;
+			for (var i = 0; i < faces.length; i++) {
+				for (var j = 0; j < faces[i].length; j++) {
+					if (j === 0) {
+						MoveTo(ctx, faces[i][j]);
+					} else if (j + 1 === faces[i].length) {
+						LineTo(ctx, faces[i][j]);
+						// LineTo(ctx, faces[i][0]);
+					} else {
+						LineTo(ctx, faces[i][j])
+					}
+				}
+			}
+			ctx.stroke();
+		};
+	//
+//
+}
+
+function CubeA () {
+// PROPERTIES
+	var pvt = {
+		x: 0,
+		y: 0,
+		z: 0
+	};
+	var angle = {
+		x:18,
+		y:0,
+		z:0
+	};
+	var thickness = 0.6;
+	var faces = [
+		[0, 2],
+		[1, 3],
+		[5, 7],
+		[4, 6],
+		[6, 2],
+		[2, 3],
+		[3, 7],
+		[7, 6],
+		[4, 0],
+		[0, 1],
+		[1, 5],
+		[5, 4]
+	];
+	var offsets = [];
+
+	var position;
+	var size;
+	var dimensions;
+//
+// METHODS
+	this.ReInit = function () {
+		position = {
+			x: config.screenWidth / 2,
+			y: config.screenHeight / 2,
+			z: (2000 - Math.min(config.screenWidth, config.screenHeight)) / 400// Math.min(config.screenWidth, config.screenHeight) / 200
+		};
+		size = Math.min(config.screenWidth, config.screenHeight) / 10;//60; //
+		dimensions = [
+			{x: size, y: size, z:-size},
+			{x: size, y: size, z: size},
+			{x: size, y:-size, z:-size},
+			{x: size, y:-size, z: size},
+			{x:-size, y: size, z:-size},
+			{x:-size, y: size, z: size},
+			{x:-size, y:-size, z:-size},
+			{x:-size, y:-size, z: size}
+		];
+			// cube.SetScale((2000 - Math.min(config.screenWidth, config.screenHeight)) / 400);
+	};
+	this.ReInit();
+	// INPUTS
+		this.MoveLeft = function (dist) {
+			position.x -= dist;
+		};
+		this.MoveUp = function (dist) {
+			position.y -= dist;
+		};
+		this.MoveRight = function (dist) {
+			position.x += dist;
+		};
+		this.MoveDown = function (dist) {
+			position.y += dist;
+		};
+		this.ScaleUp = function (zoom) {
+			position.z /= zoom;
+		};
+		this.ScaleDown = function (zoom) {
+			position.z *= zoom;
+		};
+		this.SetScale = function (val) {
+			position.z = val;
+		}
+		this.TurnX = function (deg) {
+			angle.x += deg;
+		};
+		this.TurnY = function (deg) {
+			angle.y += deg
+		};
+		this.TurnZ = function (deg) {
+			angle.z += deg
+		};
+		this.SetPosX = function (val) {
+			position.x = val;
+		}
+		this.ThicknessUp = function (val) {
+			thickness *= val;
+		}
+		this.ThicknessDown = function (val) {
+			thickness /= val;
+		}
+	//
+	// UPDATE
+		var GetOffset = function (dim) {
+			var XD = dim.x - pvt.x;
+			var YD = dim.y - pvt.y;
+			var ZD = dim.z - pvt.z;
+			var ZX = XD * Math.cos(angle.z) - YD * Math.sin(angle.z) - XD;
+			var ZY = XD * Math.sin(angle.z) + YD * Math.cos(angle.z) - YD;
+			var YX = (XD + ZX) * Math.cos(angle.y) - ZD * Math.sin(angle.y) - (XD + ZX);
+			var YZ = (XD + ZX) * Math.sin(angle.y) + ZD * Math.cos(angle.y) - ZD;
+			var XY = (YD + ZY) * Math.cos(angle.x) - (ZD + YZ) * Math.sin(angle.x) - (YD + ZY);
+			var XZ = (YD + ZY) * 4 * Math.sin(angle.x) + (ZD + YZ) * Math.cos(angle.x) - (ZD + YZ);
 			return {
 				x: YX + ZX,
 				y: ZY + XY,
--- Game.js
+++ Game.js
@@ -1,5 +1,6 @@
 var background = new Background();
 var cube = new Cube();
+var cubeA = new CubeA();
 var font = new Font();
 
 
@@ -15,8 +16,20 @@ function Update (timer) {
 		cube.ScaleUp(1 + timer.GetSin() / 50);
 		cube.ThicknessUp(1 + Math.abs(timer.GetSin() / 50));
 	}
-	cube.SetPosX(config.screenWidth / 2 + timer.GetSin() * (Math.min(config.screenWidth, config.screenHeight) / 3.5))//200);
+	cube.SetPosX(config.screenWidth / 5 + timer.GetSin() * (Math.min(config.screenWidth, config.screenHeight) / 3.5))//200);
 	cube.Update(timer);
+	cubeA.TurnX(config.speed / 3 * timer.GetElapsedTime());
+	cubeA.TurnY(config.speed / 3 * timer.GetElapsedTime());
+	cubeA.TurnZ(config.speed / 1 * timer.GetElapsedTime());
+	if (timer.GetSin() < 0) {
+		cubeA.ScaleDown(1 + Math.abs(timer.GetSin() / 50)); // 50
+		cubeA.ThicknessDown(1 + Math.abs(timer.GetSin() / 58));
+	} else {
+		cubeA.ScaleUp(1 + timer.GetSin() / 50);
+		cubeA.ThicknessUp(18 + Math.abs(timer.GetSin() / 50));
+	}
+	cubeA.SetPosX(config.screenWidth / 6 + timer.GetSin() * (Math.min(config.screenWidth, config.screenHeight) / 3.5))//200);
+	cubeA.Update(timer);
 	background.Update(timer);
 	font.Update(timer);
 }
@@ -27,8 +40,10 @@ function Draw (canvas, timer) {
 	if (timer.GetLastSin() > timer.GetSin()) {
 		font.Draw(canvas);
 		cube.Draw(canvas);
+        cubeA.Draw(canvas);
 	} else {
 		cube.Draw(canvas);
+        cubeA.Draw(canvas);
 		font.Draw(canvas);
 	}
 }
@@ -40,6 +55,7 @@ $(document).ready(function () {
 		config.screenHeight = window.innerHeight;
 		canvas.ReInit();
 		cube.ReInit();
+		cubeA.ReInit();
 		font.ReInit();
 	}
 	var timer = new Timer();
